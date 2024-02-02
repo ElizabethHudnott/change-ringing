2f77ae7e0f23822bad045d5f5aae437fb8f5c86a
Elizabeth Hudnott <ElizabethHudnott@users.noreply.github.com>
2024-02-02 01:59:16 +0000

    Add Grandsire.

--- methods.js
+++ methods.js
@@ -1,3 +1,9 @@
+const Cover = Object.freeze({
+	NONE:		0,
+	LEFT:		1,
+	RIGHT:	2,
+	EITHER:	3,
+});
 
 // Maps time to pitch (descending from 1).
 let rows = [round(4)];
@@ -19,39 +25,40 @@ function round(n) {
 }
 
 /**
+ * Requires at least 3 bells.
  * @param {number} [numRows] How many rows of plain hunting to return or undefined to
  * produce a single complete cycle.
- * @param {boolean} [coverLeft] True if the bell which is initially in Position 0 covers,
- * false if the bell in last position covers, or undefined to decide automatically.
  * @param {number} [firstSwap] The position of the first swap, disregarding covering (0 or 1)
  * or undefined to decide automatically.
  */
-function plainHunt(previousRow, numRows, coverLeft, firstSwap) {
+function plainHunt(previousRow, numRows, cover = Cover.NONE, firstSwap = undefined) {
 	const numBells = previousRow.length;
 	let firstSwapper = 0, lastSwapper = numBells - 1;
-	if (numBells & 1) {
-		if (coverLeft === undefined) {
-			coverLeft = previousRow[0] > previousRow[numBells - 1];
+	let numBobbing = numBells;
+	if (cover !== Cover.NONE) {
+		if (cover === Cover.EITHER) {
+			cover = previousRow[0] > previousRow[numBells - 1] ? Cover.LEFT : Cover.RIGHT;
 		}
-		if (coverLeft) {
+		if (cover === Cover.LEFT) {
 			firstSwapper = 1;
 		} else {
 			lastSwapper--;
 		}
+		numBobbing--;
 	}
 	if (firstSwap === undefined) {
 		const indexOf1 = previousRow.indexOf(1);
 		firstSwap = indexOf1 === firstSwapper || indexOf1 === lastSwapper ? 0 : 1;
 	}
 	if (numRows === undefined) {
-		numRows = 2 * (lastSwapper - firstSwapper + 1);
+		numRows = 2 * numBobbing;
 	}
 
 	const output = [];
 	let row = previousRow;
 	for (let i = 0; i < numRows; i++) {
 		const nextRow = row.slice();
-		for (let j = firstSwapper + firstSwap; j < lastSwapper - firstSwap; j += 2) {
+		for (let j = firstSwapper + firstSwap; j + 1 <= lastSwapper; j += 2) {
 			nextRow[j] = row[j + 1];
 			nextRow[j + 1] = row[j];
 		}
@@ -62,40 +69,92 @@ function plainHunt(previousRow, numRows, coverLeft, firstSwap) {
 	return output;
 }
 
-function plainBob(previousRow, numRows, coverLeft, firstSwap) {
+/**
+ * Requires at least 4 bells.
+ */
+function plainBob(previousRow, numRows, cover = Cover.NONE, firstSwap = undefined) {
 	const numBells = previousRow.length;
 	let firstSwapper = 0, lastSwapper = numBells - 1;
-	if (numBells & 1) {
-		if (coverLeft === undefined) {
-			coverLeft = previousRow[0] > previousRow[numBells - 1];
+	let numBobbing = numBells;
+	if (cover !== Cover.NONE) {
+		if (cover === Cover.EITHER) {
+			cover = previousRow[0] > previousRow[numBells - 1] ? Cover.LEFT : Cover.RIGHT;
 		}
-		if (coverLeft) {
+		if (cover === Cover.LEFT) {
 			firstSwapper = 1;
 		} else {
 			lastSwapper--;
 		}
+		numBobbing--;
 	}
 	if (firstSwap === undefined) {
 		const indexOf1 = previousRow.indexOf(1);
 		firstSwap = indexOf1 === firstSwapper || indexOf1 === lastSwapper ? 0 : 1;
 	}
 	if (numRows === undefined) {
-		numRows = factorial(lastSwapper - firstSwapper + 1);
+		numRows = 2 * numBobbing * (numBobbing - 1);
 	}
 
 	const output = [];
 	let row = previousRow;
 	for (let i = 0; i < numRows; i++) {
 		const nextRow = row.slice();
-		let lowerSwap, upperSwap;
+		let lowerSwap;
 		if (row[firstSwapper] === 1 && firstSwap === 1) {
 			lowerSwap = firstSwapper + 2;
-			upperSwap = lastSwapper - 1;
 		} else {
 			lowerSwap = firstSwapper + firstSwap;
-			upperSwap = lastSwapper - firstSwap - 1;
 		}
-		for (let j = lowerSwap; j <= upperSwap; j += 2) {
+		for (let j = lowerSwap; j + 1 <= lastSwapper; j += 2) {
+			nextRow[j] = row[j + 1];
+			nextRow[j + 1] = row[j];
+		}
+		output.push(nextRow);
+		row = nextRow;
+		firstSwap = 1 - firstSwap;
+	}
+	return output;
+}
+
+/**
+ * Requires at least 4 bells.
+ */
+function grandsire(previousRow, numRows, cover = Cover.NONE, firstSwap = undefined) {
+	const numBells = previousRow.length;
+	let firstSwapper = 0, lastSwapper = numBells - 1;
+	let numBobbing = numBells;
+	if (cover !== Cover.NONE) {
+		if (cover === Cover.EITHER) {
+			cover = previousRow[0] > previousRow[numBells - 1] ? Cover.LEFT : Cover.RIGHT;
+		}
+		if (cover === Cover.LEFT) {
+			firstSwapper = 1;
+		} else {
+			lastSwapper--;
+		}
+		numBobbing--;
+	}
+	if (firstSwap === undefined) {
+		const indexOf1 = previousRow.indexOf(1);
+		firstSwap = indexOf1 === firstSwapper || indexOf1 === lastSwapper ? 0 : 1;
+	}
+	if (numRows === undefined) {
+		numRows = 2 * numBobbing * (numBobbing - 2);
+	}
+
+	const output = [];
+	let row = previousRow;
+	for (let i = 0; i < numRows; i++) {
+		const nextRow = row.slice();
+		let lowerSwap;
+		if (row[firstSwapper] === 1 && firstSwap === 0) {
+			nextRow[firstSwapper] = row[firstSwapper + 1];
+			nextRow[firstSwapper + 1] = row[firstSwapper];
+			lowerSwap = firstSwapper + 3;
+		} else {
+			lowerSwap = firstSwapper + firstSwap;
+		}
+		for (let j = lowerSwap; j + 1 <= lastSwapper; j += 2) {
 			nextRow[j] = row[j + 1];
 			nextRow[j + 1] = row[j];
 		}
